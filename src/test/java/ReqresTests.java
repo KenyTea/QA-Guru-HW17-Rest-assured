4ec38fee76a759dece74d561b37c54cf8caf359c
Vasenkov Stanislav <aanher@gmail.com>
2021-12-27 20:00:23 +0300

    Update ReqresTests.java

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -5,24 +7,30 @@ import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
 public class ReqresTests {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
     @Test
     void successfulLogin() {
-//        request https://reqres.in/api/login
-//        data  {
-//              "email": "eve.holt@reqres.in",
-//                "password": "cityslicka"
-//              }
-//        response  {
-//                  "token": "QpwL5tke4Pnpja7X4"
-//                  }
-//        status    200
+        // https://reqres.in/api/login
+        /*
+            {
+                "email": "eve.holt@reqres.in",
+                "password": "cityslicka"
+            }
+         */
+        //     "token": "QpwL5tke4Pnpja7X4"
+        //     200
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
 
         given()
                 .contentType(JSON)
-                .body("{\"email\": \"eve.holt@reqres.in\"," +
-                        "\"password\": \"cityslicka\"}")
+                .body(data)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post("/api/login")
                 .then()
                 .statusCode(200)
                 .body("token", is("QpwL5tke4Pnpja7X4"));
@@ -30,11 +38,22 @@ public class ReqresTests {
 
     @Test
     void negativeLogin() {
+        // https://reqres.in/api/login
+        /*
+            {
+                "email": "eve.holt@reqres.in"
+            }
+         */
+        //     "error": "Missing password"
+        //     400
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\"}";
+
         given()
                 .contentType(JSON)
-                .body("{\"email\": \"eve.holt@reqres.in\"}")
+                .body(data)
                 .when()
-                .post("https://reqres.in/api/login")
+                .post("/api/login")
                 .then()
                 .statusCode(400)
                 .body("error", is("Missing password"));
