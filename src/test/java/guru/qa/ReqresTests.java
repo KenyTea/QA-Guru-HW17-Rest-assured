5c22ffcfe542ff2c64600c9b5140e5f8e7d61fac
Alexey <vassilyevalexey@gmail.com>
2022-01-14 17:39:24 +0600

    Превая загрузка

---
+++ src/test/java/guru/qa/ReqresTests.java
@@ -0,0 +1,89 @@
+package guru.qa;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
+    @Test
+    void singleUser() {
+        given()
+                .when()
+                .get("api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    void listResource() {
+        given()
+                .when()
+                .get("api/unknown")
+                .then()
+                .statusCode(200)
+                .body("page", is(1))
+                .body("total", is(12))
+                .body("data[0].id", is(1))
+                .body("data[1].name", is("fuchsia rose"))
+                .body("data[2].year", is(2002));
+    }
+
+    @Test
+    void listResourceWithFor() {
+        Response data =
+                given()
+                        .when()
+                        .get("api/unknown")
+                        .then()
+                        .extract().response();
+        ArrayList<Object> dataList = data.path("data");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("cerulean");
+        names.add("fuchsia rose");
+        names.add("true red");
+        names.add("aqua sky");
+        names.add("tigerlily");
+        names.add("blue turquoise");
+        int i = 0;
+        for (Object x : dataList) {
+            if (i + 1 == (int) ((LinkedHashMap) x).get("id")) {
+                Assertions.assertEquals(((LinkedHashMap) x).get("name"), names.get(i));
+            }
+            i++;
+        }
+    }
+
+    @Test
+    void singleResource() {
+
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+}
